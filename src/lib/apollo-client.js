a49031e02799ab18e4a33fcd556628c812f24689
john fellman <john.fellman@gmail.com>
2017-07-13 09:08:22 -0700

    fix(apollo) clean up APP_URL usage

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -3,7 +3,7 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import { APP_URL } from '../../config'
 
 const networkInterface = createNetworkInterface({
-    uri: APP_URL
+    uri: `${APP_URL}/graphql`
 })
 
 export default new ApolloClient({ networkInterface })
